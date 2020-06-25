0fd85d8f626255470eb2434edacded7800346994
elbear47 <elberfunez@gmail.com>
2020-06-25 00:30:01 -0400

    Now printing route for truck onee

---
+++ Item.py
@@ -0,0 +1,17 @@
+import datetime
+
+# this is the time that the first truck leaves the hub
+first_time_list = ['8:00:00']
+
+def check_time_first_truck(distance):
+    new_time = distance / 18
+    distance_in_minutes = '{0:02.0f}:{1:02.0f}'.format(*divmod(new_time * 60, 60))
+    final_time = distance_in_minutes + ':00'
+    first_time_list.append(final_time)
+    sum = datetime.timedelta()
+    for i in first_time_list:
+        (h, m, s) = i.split(':')
+        d = datetime.timedelta(hours=int(h), minutes=int(m), seconds=int(s))
+        sum += d
+    return sum
+
